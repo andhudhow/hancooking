5dd303b1db8949686b058ca72c6cffb4a05f9a30
Andrew Howell <andhudhow@gmail.com>
2020-03-07 11:42:04 -0800

    Add a test commetn

--- frontend/components/recipe_box/recipe_card.jsx
+++ frontend/components/recipe_box/recipe_card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//comment
+//comment with some more changes
 
 class RecipeCard extends React.Component{
   constructor(props) {
