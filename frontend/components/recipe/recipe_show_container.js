ddd2bedb3e26ee2cfa28e1bc12bdc4558eb311aa
Andrew Howell <andhudhow@gmail.com>
2020-04-01 09:32:48 -0700

    clean up return on show container

--- frontend/components/recipe/recipe_show_container.js
+++ frontend/components/recipe/recipe_show_container.js
@@ -6,17 +6,14 @@ import { withRouter } from 'react-router-dom';
 
 
 
-const mapStateToProps = ({ session, entities}, ownProps) => {
-  
-  return ({
+const mapStateToProps = ({ session, entities}, ownProps) => ({
     currentUser: session.currentUser,
     recipe: entities.recipes[ownProps.match.params.recipeId],
     ingredients: Object.keys(entities.ingredients).map(key=>entities.ingredients[key]),
     prepSteps: Object.keys(entities.prepSteps).map(key=>entities.prepSteps[key]),
     comments: Object.keys(entities.comments).map(key=>entities.comments[key]).sort((a, b) => (a.createdAt > b.createdAt) ? -1 : 1),
     ratings: Object.keys(entities.ratings).map(key=>entities.ratings[key]),
-  })
-};
+});
   
 const mapDispatchToProps = dispatch => ({
   fetchRecipe: recipeId => dispatch(fetchRecipe(recipeId)),
